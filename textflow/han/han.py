6dfb0df977658064898c47461f39e4ea09cf13fb
Johan Attia <attia.johan@gmail.com>
2021-02-21 02:17:50 +0100

    begin skipgram architecture script

--- textflow/han/han.py
+++ textflow/han/han.py
@@ -1,3 +1,35 @@
-import tensorflow as tf
+"""Hierarchical Attention Networks for TensorFlow."""
 
-# TODO: implement Hierarchical Attention Networks with TensorFlow
+import tensorflow as tf
+from tensorflow_addons.utils.types import FloatTensorLike
+
+from typeguard import typechecked
+
+
+class HierarchicalAttentionNetwork(tf.keras.Model):
+    """Hierarchical Attention Network for TensorFlow.
+
+    Title: Hierarchical Attention Networks for Documents Classification
+    Link: https://www.cs.cmu.edu/~./hovy/papers/16HLT-hierarchical-attention-networks.pdf
+
+    Example of usage:
+    ```python
+    import textflow
+
+    han = textflow.han.HierarchicalAttentionNetwork()
+    han.compile(loss="categorical_crossentropy", optimizer="adam")
+    ```
+    """
+
+    def __init__(self):
+        super(HierarchicalAttentionNetwork, self).__init__()
+
+    def call(self, inputs):
+        pass
+
+    @staticmethod
+    def prepare_dataset(self):
+        pass
+
+    def get_config(self):
+        pass
--- textflow/skipgram/skipgram.py
+++ textflow/skipgram/skipgram.py
@@ -1,3 +1,78 @@
-import tensorflow as tf
+"""Skipgram for TensorFlow."""
 
-# TODO: implement Skipgram with TensorFlow
+from typing import Optional
+from typeguard import typechecked
+
+import tensorflow as tf
+from tensorflow_addons.utils.types import FloatTensorLike, Number
+
+# TODO: train_step, test_step, inference...
+
+
+class Skipgram(tf.keras.Model):
+    """Negative Sampling Skipgram for TensorFlow.
+
+    References:
+    * Distributed Representations of Words and Phrases and their Compositionality, https://arxiv.org/pdf/1310.4546.pdf
+    * Enriching Word Vectors with Subword Information, https://arxiv.org/pdf/1607.04606.pdf
+    * Advances in Pre-Training Distributed Word Representations, https://arxiv.org/pdf/1712.09405.pdf
+
+    Example of usage:
+    ```python
+    import textflow
+
+    skipgram = textflow.skipgram.Skipgram(dimension=300, vocab_size=12000)
+    dataset = skipgram.prepare_dataset(documents)
+    skipgram.compile(optimizer="adam")
+
+    history = skipgram.fit(dataset, epoch=10)
+    ```
+    """
+
+    @typechecked
+    def __init__(
+        self,
+        dimension: Number = 300,
+        vocab_size: Number = None,
+        window_size: Number = 4,
+        negative_samples: Number = 1,
+        **kwargs
+    ):
+        super(Skipgram, self).__init__(**kwargs)
+        self.dimension = dimension
+        self.vocab_size = vocab_size
+        self.window_size = window_size
+        self.negative_samples = negative_samples
+
+        self.target_embedding = tf.keras.layers.Embedding(
+            self.vocab_size, self.dimension, name="skipgram_embedding"
+        )
+        self.context_embedding = tf.keras.layers.Embedding(
+            self.vocab_size, self.dimension, name="context_embedding"
+        )
+
+    def call(self, inputs: FloatTensorLike) -> FloatTensorLike:
+        target_word, context_word = inputs
+        target_vector = self.target_embedding(target_word)
+        context_vector = self.context_embedding(context_word)
+        dot_product = tf.math.reduce_sum(
+            tf.multiply(target_vector, context_vector), axis=1
+        )
+        return tf.nn.sigmoid(dot_product)  # loss : from_logits=False
+
+    def prepare_dataset(
+        self,
+        sampling_table: Optional = None,
+        shuffle: bool = True,
+        categorical: bool = False,
+    ) -> tf.data.Dataset:
+        pass
+
+    def get_config(self) -> dict:
+        config = {
+            "dimension": self.dimension,
+            "vocab_size": self.vocab_size,
+            "window_size": self.window_size,
+            "negative_samples": self.negative_samples,
+        }
+        return config
