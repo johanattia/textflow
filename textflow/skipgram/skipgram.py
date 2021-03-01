7b55693791e8a384ac7f639ad35433dab1b577e3
Johan Attia <attia.johan@gmail.com>
2021-03-02 00:04:32 +0100

    skipgram call almost done  and TODO

--- textflow/skipgram/skipgram.py
+++ textflow/skipgram/skipgram.py
@@ -6,7 +6,7 @@ from typeguard import typechecked
 import tensorflow as tf
 from tensorflow_addons.utils.types import FloatTensorLike, Number
 
-# TODO: train_step, test_step, inference...
+# TODO: call method, similarity, prepare_dataset, pretrained_weights, tokenizer, fit, get_config...
 
 
 class Skipgram(tf.keras.Model):
@@ -21,45 +21,77 @@ class Skipgram(tf.keras.Model):
     ```python
     import textflow
 
-    skipgram = textflow.skipgram.Skipgram(dimension=300, vocab_size=12000)
-    dataset = skipgram.prepare_dataset(documents)
-    skipgram.compile(optimizer="adam")
+    w2v = textflow.skipgram.Skipgram(dimension=300, vocab_size=12000)
+    dataset = w2v.prepare_dataset(documents)
+    w2v.compile(optimizer="adam")
 
-    history = skipgram.fit(dataset, epoch=10)
+    history = w2v.fit(dataset, epoch=10)
     ```
     """
 
     @typechecked
     def __init__(
         self,
-        dimension: Number = 300,
-        vocab_size: Number = None,
+        vocab_size: Number,
+        embedding_dim: Number,
         window_size: Number = 4,
         negative_samples: Number = 1,
         **kwargs
     ):
-        super(Skipgram, self).__init__(**kwargs)
-        self.dimension = dimension
+        """Skigpram class constructor.
+
+        Arguments:
+            vocab_size: int.
+                Size of the vocabulary.
+            embedding_dim: int.
+                Dimension of trained word2vec Skipgram embeddings.
+
+        """
+        # Inheritance
+        super(Skipgram, self).__init__()
+
+        # Skipgram parameters
         self.vocab_size = vocab_size
+        self.embedding_dim = embedding_dim
         self.window_size = window_size
         self.negative_samples = negative_samples
 
+        # Layers
         self.target_embedding = tf.keras.layers.Embedding(
-            self.vocab_size, self.dimension, name="skipgram_embedding"
+            self.vocab_size,
+            self.embedding_dim,
+            input_length=1,
+            name="w2v_embedding",
         )
         self.context_embedding = tf.keras.layers.Embedding(
-            self.vocab_size, self.dimension, name="context_embedding"
+            self.vocab_size,
+            self.embedding_dim,
+            input_length=4 + 1,  # number of negative samples = 4
         )
-        self.dots = tf.keras.layers.Dot()
+        self.dots = tf.keras.layers.Dot(axes=(3, 2))
+        self.flatten = tf.keras.layers.Flatten()
 
-    def call(self, inputs: FloatTensorLike) -> FloatTensorLike:
-        target_word, context_word = inputs
-        target_vector = self.target_embedding(target_word)
-        context_vector = self.context_embedding(context_word)
-        dot_product = tf.math.reduce_sum(
-            tf.multiply(target_vector, context_vector), axis=1
-        )
-        return tf.nn.sigmoid(dot_product)  # loss : from_logits=False
+    def call(self, inputs, training: bool = None, mask: FloatTensorLike = None):
+        """Model forward method.
+
+        Arguments:
+            inputs: FloatTensorLike. Pair of (center_word, context_words).
+                Context words contains both positive and negative examples.
+            training: bool. Indicating whether to run the `Network` in training mode or inference mode.
+                Useless here.
+            mask: FloatTensorLike. Mask or list of masks.
+                Useless here.
+
+        Returns:
+            FloatTensorLike. Dot products between center word and context words.
+        """
+        target, context = inputs
+
+        we = self.target_embedding(target)
+        ce = self.context_embedding(context)
+        dots = self.dots([ce, we])
+
+        return self.flatten(dots)
 
     def prepare_dataset(
         self,
@@ -71,54 +103,9 @@ class Skipgram(tf.keras.Model):
 
     def get_config(self) -> dict:
         config = {
-            "dimension": self.dimension,
+            "embedding_dim": self.embedding_dim,
             "vocab_size": self.vocab_size,
             "window_size": self.window_size,
             "negative_samples": self.negative_samples,
         }
         return config
-
-
-# class Skipgram(tf.keras.Model):
-#    """Negative Sampling Skigpram implementation.
-#
-#    ```python
-#    w2v = Skipgram(vocab_size=4096, embedding_dim=128)
-#    ````
-#    """
-#    def __init__(self, vocab_size, embedding_dim):
-#        """Skigpram class constructor.
-#
-#        Parameters
-#        ----------
-#        vocab_size: int.
-#            Size of the vocabulary.
-#
-#        embedding_dim: int.
-#            Dimension of trained word2vec Skipgram embeddings.
-#
-#        """
-#        super(Skipgram, self).__init__()
-#        self.target_embedding = tf.keras.layers.Embedding(
-#            vocab_size,
-#            embedding_dim,
-#            input_length=1,
-#            name="w2v_embedding",
-#        )
-#        self.context_embedding = tf.keras.layers.Embedding(
-#            vocab_size,
-#            embedding_dim,
-#            input_length=4+1 # number of negative samples = 4
-#        )
-#        self.dots = tf.keras.layers.Dot(axes=(3,2))
-#        self.flatten = tf.keras.layers.Flatten()
-#
-#    def call(self, pair):
-#        """Model forward method.
-#        """
-#        target, context = pair
-#        we = self.target_embedding(target)
-#        ce = self.context_embedding(context)
-#        dots = self.dots([ce, we])
-#
-#        return self.flatten(dots)
