7caf72b9dff0baa5caa7450ca35de274d92af032
Johan Attia <attia.johan@gmail.com>
2021-04-13 01:36:45 +0200

    skipgram : manage oov words for sentence embedding and similarity methods

--- textflow/embedding/skipgram.py
+++ textflow/embedding/skipgram.py
@@ -1,13 +1,13 @@
 """Skipgram for TensorFlow."""
 
-from typing import Iterable, Tuple, Union  # Optional
+from typing import Iterable, Optional, Tuple, Union
 from typeguard import typechecked
 
 import numpy as np
 from annoy import AnnoyIndex
 
 import tensorflow as tf
-from tensorflow_addons.utils.types import TensorLike  # FloatTensorLike
+from tensorflow_addons.utils.types import TensorLike
 
 from tqdm.auto import tqdm
 
@@ -25,8 +25,8 @@ class VocabularyError(LookupError):
 def make_skipgram_dataset(
     texts: Iterable[str],
     window_size: int = 4,
-    negative_samples: int = 1,
-    buffer_size: int = None,
+    negative_samples: Optional[int] = 2,
+    buffer_size: Optional[int] = None,
 ) -> Tuple[tf.data.Dataset, tf.keras.preprocessing.text.Tokenizer]:
     """Build skipgram dataset and tokenizer from texts. A skipgram dataset is composed
     of skipgram pairs (center_word, context_words). Note that context_words may
@@ -37,12 +37,12 @@ def make_skipgram_dataset(
             A corpus of texts.
 
         window_size: int, default to 4.
-        Sliding window for skipgram dataset building.
+            Sliding window for skipgram dataset building.
 
-        negative_samples: int, default to 1.
+        negative_samples: Optional[int], default to 1.
             Number of negative samples to generate for each target_word.
 
-        buffer_size: int, default to None.
+        buffer_size: Optional[int], default to None.
             Buffer size for tf.data.Dataset shuffling.
 
     Returns:
@@ -73,14 +73,13 @@ class Skipgram(tf.keras.Model):
     import textflow
 
     dimension = 128
-    word2vec_dataset, tokenizer = textflow.embedding.Skipgram.prepare_dataset(texts)
+    word2vec_dataset, tokenizer = textflow.embedding.make_skipgram_dataset(texts)
 
     word2vec = textflow.embedding.Skipgram(dimension=dimension, tokenizer=tokenizer)
     word2vec.compile(
         optimizer="adam",
         loss=tf.keras.losses.BinaryCrossentropy(from_logits=True)
     )
-
     history = word2vec.fit(word2vec_dataset, epoch=20)
     ```
     """
@@ -149,7 +148,7 @@ class Skipgram(tf.keras.Model):
         """Model forward method.
 
         Args:
-            inputs: pair of int tensors-like (center_word, context_words).
+            inputs: tuple of index tensors (center_word, context_words).
                 Context words contains both positive and negative examples.
 
         Returns:
@@ -167,12 +166,12 @@ class Skipgram(tf.keras.Model):
 
         return products  # tf.nn.softmax/sigmoid/sigmoid_cross_entropy_with_logits
 
-    def predict_step(self, data: Union[TensorLike, Iterable[TensorLike]]) -> tf.Tensor:
+    def predict_step(self, data: Union[TensorLike, Tuple[TensorLike]]) -> tf.Tensor:
         """Model inference step. Overrides and follows tf.keras.Model predict_step method:
         https://github.com/tensorflow/tensorflow/blob/v2.4.1/tensorflow/python/keras/engine/training.py#L1412-L1434
 
         Args:
-            data: tuple of int tensors-like (center_word, context_words).
+            data: tuple of tensors (center_word, context_words, weights) or just (center) word index tensor.
                 Context words contains both positive and negative examples.
 
         Returns:
@@ -195,7 +194,7 @@ class Skipgram(tf.keras.Model):
 
         return self.skipgram_embedding(x)
 
-    def word_vector(self, words: Iterable[str]) -> tf.Tensor:
+    def word_vectors(self, words: Iterable[str]) -> tf.Tensor:
         """Getting word vector method.
 
         Args:
@@ -208,12 +207,12 @@ class Skipgram(tf.keras.Model):
 
         Raises:
             VocabularyError.
-                If word is out-of-vocabulary.
+                If one or several word from words argument are out-of-vocabulary.
         """
         indexes = tf.convert_to_tensor(
             self.tokenizer.texts_to_sequences(words), dtype=tf.int32
         )
-        oov_mask = tf.equal(indexes, tf.ones(indexes.shape, dtype=tf.int32))
+        oov_mask = tf.equal(indexes, tf.ones_like(indexes, dtype=tf.int32))
 
         if tf.reduce_any(oov_mask):
             oov_words = (
@@ -225,33 +224,50 @@ class Skipgram(tf.keras.Model):
 
         return tf.squeeze(self.predict(indexes), axis=1)
 
-    def sentence_vector(
-        self, sentence: Iterable[str], ignore_oov: bool = True
+    def sentence_vectors(
+        self, sentences: Iterable[str], ignore_oov: bool = True
     ) -> tf.Tensor:
         """Getting sentence vector method through word vector averaging.
-        Since some words may not be taken into account by the tokenizer,
 
         Args:
-            sentence: Iterable[str].
-                Sentence(s) for which vector(s)/embedding(s) is/are returned.
+            sentences: Iterable[str].
+                Sentences for which vectors/embeddings are returned.
+
+            ignore_oov: bool, default to True.
+                Consider a sentence of n tokens composed of n_i in-vocabulary tokens and
+                n_o oov tokens (n_i + n_o = n), two cases proposed:
+                - ignore_oov=True : word vector averaging will be made by suming the n_i in-vocabulary
+                word vectors and dividing by n.
+                - ignore_oov=False : word vector averaging will be made by replacing the n_o oov word
+                vectors by a special oov vector.
 
         Returns:
             tensor: tf.Tensor.
                 Average skipgram embedding(s) for sentence.
         """
         indexes = tf.ragged.constant(
-            self.tokenizer.texts_to_sequences(sentence), dtype=tf.int32
+            self.tokenizer.texts_to_sequences(sentences), dtype=tf.int32
         )
-        sentence_vector = tf.reduce_mean(self.predict(indexes), axis=1)
-        return sentence_vector
+
+        if ignore_oov:
+            oov_mask = tf.equal(indexes, tf.ones_like(indexes, dtype=tf.int32))
+
+            filtered_indexes = tf.ragged.boolean_mask(indexes, ~oov_mask)
+            filtered_vectors = self.predict(filtered_indexes)
+
+            return tf.reduce_sum(filtered_vectors, axis=1) / tf.cast(
+                tf.expand_dims(indexes.row_lengths(axis=-1), axis=-1), dtype=tf.float32
+            )
+
+        return tf.reduce_mean(self.predict(indexes), axis=1)
 
     def word_similarity(
-        self, word1: Iterable[str], word2: Iterable[str], ignore_oov: bool = True
+        self, words1: Iterable[str], words2: Iterable[str]
     ) -> tf.Tensor:
         """Compute cosine similarity for respective pairs of words.
-        word1 and word2 arguments are iterable and must have the same length.
+        words1 and words2 arguments are iterable and must have the same length.
 
-        For instance, let's consider word1 = [w11, w12, ..., w1n] and word2 = [w21, w22, ..., w2n],
+        For instance, let's consider words1 = [w11, w12, ..., w1n] and words2 = [w21, w22, ..., w2n],
         this method will compute [cosine_similarity(w11, w21), ..., cosine_similarity(w1n, w2n)].
 
         Args:
@@ -265,39 +281,39 @@ class Skipgram(tf.keras.Model):
             tensor: tf.Tensor.
                 Cosine similarity tensor.
         """
-        word_vector1 = self.word_vector(word1)
-        word_vector2 = self.word_vector(word2)
+        word_vectors1 = self.word_vector(words1)
+        word_vectors2 = self.word_vector(words2)
 
-        return -tf.keras.losses.cosine_similarity(word_vector1, word_vector2, axis=1)
+        return -tf.keras.losses.cosine_similarity(word_vectors1, word_vectors2, axis=1)
 
     def sentence_similarity(
         self,
-        sentence1: Iterable[str],
-        sentence2: Iterable[str],
+        sentences1: Iterable[str],
+        sentences2: Iterable[str],
         ignore_oov: bool = True,
     ) -> tf.Tensor:
         """Compute cosine similarity for respective pairs of sentences.
-        sentence1 and sentence2 arguments are iterable and must have the same length.
+        sentences1 and sentences2 arguments are iterable and must have the same length.
 
-        For instance, let's consider sentence1 = [s11, s12, ..., s1n] and sentence2 = [s21, s22, ..., s2n],
+        For instance, let's consider sentences1 = [s11, s12, ..., s1n] and sentences2 = [s21, s22, ..., s2n],
         this method will compute [cosine_similarity(s11, s21), ..., cosine_similarity(s1n, s2n)].
 
         Args:
-            sentence1: Iterable[str].
+            sentences1: Iterable[str].
                 First sentences of sentence pairs.
 
-            sentence2: Iterable[str].
+            sentences2: Iterable[str].
                 Second sentences of sentence pairs.
 
         Returns:
             tensor: tf.Tensor.
                 Cosine similarity tensor.
         """
-        sentence_vector1 = self.sentence_vector(sentence1)
-        sentence_vector2 = self.sentence_vector(sentence2)
+        sentence_vectors1 = self.sentence_vectors(sentences1, ignore_oov=ignore_oov)
+        sentence_vectors2 = self.sentence_vectors(sentences2, ignore_oov=ignore_oov)
 
         return -tf.keras.losses.cosine_similarity(
-            sentence_vector1, sentence_vector2, axis=1
+            sentence_vectors1, sentence_vectors2, axis=1
         )
 
     def create_index(
@@ -317,6 +333,11 @@ class Skipgram(tf.keras.Model):
                 Specifies the number of threads used to build the trees.
                 n_jobs=-1 uses all available CPU cores.
         """
+        if self.search_index:
+            raise AttributeError(
+                "Already existed search index for approximate vector search."
+            )
+
         search_index = AnnoyIndex(self.dimension, metric=metric)
 
         for index in tqdm(
