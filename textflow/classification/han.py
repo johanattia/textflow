540095b0e909de1747e122c353438da5092fc813
Johan Attia <attia.johan@gmail.com>
2021-05-07 09:25:16 +0200

    update sentence and document encoders methods in HAN

--- textflow/classification/han.py
+++ textflow/classification/han.py
@@ -1,10 +1,11 @@
 """Hierarchical Attention Networks for TensorFlow."""
 
-from typing import Iterable
+from typing import Union
 from typeguard import typechecked
 
+import numpy as np
 import tensorflow as tf
-from tensorflow_addons.utils.types import FloatTensorLike
+from tensorflow_addons.utils.types import FloatTensorLike, TensorLike
 
 from .attention import AttentionLayer
 
@@ -36,25 +37,36 @@ class HierarchicalAttentionNetwork(tf.keras.Model):
         self,
         vocab_size: int,
         embedding_dim: int,
-        pretrained_weights: FloatTensorLike,
         gru_units: int,
         attention_units: int,
         classifier_units: int,
+        initializer: Union[str, tf.keras.initializers.Initializer] = "uniform",
     ):
-        """Hierarchical Attention Networks constructor.
+        """Hierarchical Attention Network class constructor.
 
         Args:
             vocab_size (int): [description]
             embedding_dim (int): [description]
-            pretrained_weights (FloatTensorLike): [description]
             gru_units (int): [description]
             attention_units (int): [description]
             classifier_units (int): [description]
+            initializer (Union[str, tf.keras.initializers.Initializer], optional): [description]. Defaults to "uniform".
         """
         super(HierarchicalAttentionNetwork, self).__init__()
+
         self.embedding = tf.keras.layers.Embedding(
-            vocab_size, embedding_dim, weights=[pretrained_weights], trainable=True
+            vocab_size,
+            embedding_dim,
+            embeddings_initializer=initializer,
+            trainable=True,
         )
+        self.WordGRU = tf.keras.layers.Bidirectional(
+            tf.keras.layers.GRU(
+                units=gru_units, activation="tanh", return_sequences=True
+            ),
+            merge_mode="concat",
+        )
+        self.WordAttention = AttentionLayer(units=attention_units)
         self.SentenceGRU = tf.keras.layers.Bidirectional(
             tf.keras.layers.GRU(
                 units=gru_units, activation="tanh", return_sequences=True
@@ -62,31 +74,52 @@ class HierarchicalAttentionNetwork(tf.keras.Model):
             merge_mode="concat",
         )
         self.SentenceAttention = AttentionLayer(units=attention_units)
-        self.DocumentGRU = tf.keras.layers.Bidirectional(
-            tf.keras.layers.GRU(
-                units=gru_units, activation="tanh", return_sequences=True
-            ),
-            merge_mode="concat",
-        )
-        self.DocumentAttention = AttentionLayer(units=attention_units)
         self.fc = tf.keras.layers.Dense(units=classifier_units)
 
-    def call(self, x):
-        """Model forward method."""
-        sentences_vectors, _ = self.sentence_encoder(x)
-        sentences_vectors = self.DocumentGRU(sentences_vectors)
-        document_vector, _ = self.DocumentAttention(sentences_vectors)
+    def call(self, x: TensorLike) -> FloatTensorLike:
+        """Model forward method.
 
+        Args:
+            x (TensorLike): [description]
+
+        Returns:
+            FloatTensorLike: [description]
+        """
+        sentences_vectors, _ = self.word_to_sentence_encoder(x)
+        document_vector, _ = self.sentence_to_document_encoder(sentences_vectors)
         return self.fc(document_vector)
 
-    def sentence_encoder(self, x):
-        """Sentence encoder with Bidirectional GRU and Attention."""
+    def word_to_sentence_encoder(self, x: TensorLike) -> FloatTensorLike:
+        """Given words from each sentences, encode the contextual representation of
+        the words from the sentence with Bidirectional GRU and Attention, and output
+        a sentence_vector.
+
+        Args:
+            x (TensorLike): [description]
+
+        Returns:
+            FloatTensorLike: [description]
+        """
         x = self.embedding(x)
-        x = tf.keras.layers.TimeDistributed(self.SentenceGRU)(x)
-        context_vector, attention_weights = self.SentenceAttention(x)
+        x = tf.keras.layers.TimeDistributed(self.WordGRU)(x)
+        context_vector, attention_weights = self.WordAttention(x)
 
         return context_vector, attention_weights
 
-    def document_encoder(self, x, return_weights=True):
-        """Document encoder using overall hierarchical architecture."""
-        pass
+    def sentence_to_document_encoder(
+        self, sentences_vectors: FloatTensorLike
+    ) -> FloatTensorLike:
+        """Given sentences from each review, encode the contextual representation of
+        the sentences with Bidirectional GRU and Attention, and output
+        a document vector.
+
+        Args:
+            sentences_vectors (FloatTensorLike): [description]
+
+        Returns:
+            FloatTensorLike: [description]
+        """
+        sentences_vectors = self.SentenceGRU(sentences_vectors)
+        document_vector, attention_weights = self.SentenceAttention(sentences_vectors)
+
+        return document_vector, attention_weights
